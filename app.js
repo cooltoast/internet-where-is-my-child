a749e44cda372a1e8c4681cfa091329bc0b60bfd
cooltoast <mpatel678@gmail.com>
2014-05-06 17:07:32 -0400

    use postgres db

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require("express");
 var ejs = require("ejs");
+var pg = require('pg');
 var app = express();
-
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('coordinates.db');
 
 ///////////////////////////////////////////////////////////////////////////////
 // APP CONFIGURATION                                                         //
@@ -15,18 +13,18 @@ app.use('/static', express.static(__dirname + '/static'));
 //use ejs for html templates
 app.engine('html', ejs.renderFile);
 
-
-
 ///////////////////////////////////////////////////////////////////////////////
 // APP ROUTES                                                                //
 ///////////////////////////////////////////////////////////////////////////////
 //default route
 app.get('/', function(req, res) {
-  db.all('SELECT * FROM coordinate_table', function(err, items) {
-   // render page here
-   console.log(items);
-
-   res.render('index.html', { items: items });
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM coordinate_table', function(err, items) {
+      if(err) return console.error(err);
+      console.log(items.rows);
+      done();
+      res.render('index.html', { items: items.rows });
+    });
   });
 });
 
@@ -34,9 +32,15 @@ app.get('/submit_coordinates', function(req, res) {
   var latitude = req.query['latitude'];
   var longitude = req.query['longitude'];
   var timestamp = req.query['timestamp'];
-  db.run('INSERT INTO coordinate_table VALUES (' + latitude + "," + longitude + ',"' + timestamp + '")');
-  
-  res.redirect('/');
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('INSERT INTO coordinate_table VALUES (' + latitude + "," + longitude + ",'" + timestamp + "')", function(err, items) {
+      if(err) return console.error(err);
+      console.log(items.rows);
+      done();
+      res.redirect('/');
+    });
+  });
 });
 
 
