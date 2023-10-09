52f14555cd91a366a40266bb4f1e347f617b5998
cooltoast <mpatel@innovikas.com>
2023-10-09 14:44:58 -0700

    delete useless stuff

--- create_db.js
+++
@@ -1,8 +0,0 @@
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('coordinates.db');
-
-db.serialize(function() {
-  db.run('CREATE TABLE coordinate_table (latitude REAL, longitude REAL, timestamp TEXT)');
-  db.run('INSERT INTO coordinate_table VALUES (45.6, 43.6, "sql test 1")');
-
-});
